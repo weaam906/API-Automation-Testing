dbc62a9f666588454ff54e7cb4dc6729311a5227
weaam kamal <weamkamal450@gmail.com>
2023-02-13 20:20:01 +0200

    project skeleton

---
+++ API Automation Testing/src/com/testing/View/Runner.java
@@ -0,0 +1,10 @@
+package com.testing.View;
+
+public class Runner {
+    public static void main(String[] args) {
+ //Personal key
+        // f4002854f34c545a2abd2e016718c28a
+ //Token
+        //ATTA5ee137a59b2027836e94c75acb71fe794a9d1df5cb4db9aa909f502325f83a6151E5A0A9
+    }
+}
